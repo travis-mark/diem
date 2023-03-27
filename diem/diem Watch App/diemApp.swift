704b0ca9e8a9c035a86676b11747325e9ac48e59
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2023-03-26 21:47:40 -0400

    Add "hello diem" phone and watch apps

---
+++ diem/diem Watch App/diemApp.swift
@@ -0,0 +1,18 @@
+//
+//  diemApp.swift
+//  diem Watch App
+//
+//  Created by Travis Luckenbaugh on 3/26/23.
+//
+
+import SwiftUI
+
+
+@main
+struct diem_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ diem/diem/diemApp.swift
@@ -0,0 +1,17 @@
+//
+//  diemApp.swift
+//  diem
+//
+//  Created by Travis Luckenbaugh on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct diemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
