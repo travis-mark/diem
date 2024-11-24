b98eba047c28cc8f91bf623c4ef4a8680119f883
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2024-11-24 13:59:30 -0500

    Write custom modifier to support infinite scroll

--- diem/DiemWatchApp.swift
+++ diem/DiemWatchApp.swift
@@ -6,22 +6,33 @@ import SwiftUI
 struct ContentView: View {
     @State private var date = Date()
     @State private var offset: Double = 0
+    @State private var previousValue: Double = 0
+    @State private var loops: Double = 0
+    let minValue: Double = 0
+    let maxValue: Double = 4
     var body: some View {
         VStack(spacing: 20) {
             EverythingView(date: date)
                 .focusable(true)
-//            TODO: 2024-11-24 - Write custom modifier to support infinite scroll
-                .digitalCrownRotation($offset, from: -2, through: 2, by: 1, sensitivity: .medium, isContinuous: true, isHapticFeedbackEnabled: false)
+                .digitalCrownRotation($offset, from: minValue, through: maxValue, by: 1, sensitivity: .medium, isContinuous: true, isHapticFeedbackEnabled: false)
                 .onChange(of: offset) { newValue in
-                    guard let offsetDate = Calendar.current.date(byAdding: .day, value: Int(newValue), to: Date())  else {
+                    let delta = newValue - previousValue
+                    if abs(delta) > (maxValue - minValue) * 0.5 {
+                        loops += delta < 0 ? 1 : -1
+                    }
+                    let newOffset = newValue + (maxValue - minValue) * loops
+                    guard let offsetDate = Calendar.current.date(byAdding: .day, value: Int(newOffset), to: Date()) else {
                         return
                     }
                     date = offsetDate
+                    previousValue = newValue
                 }
             if offset != 0 {
                 Button(action: {
                     date = Date()
                     offset = 0
+                    previousValue = 0
+                    loops = 0
                 }) {
                     Text("Reset")
                 }
